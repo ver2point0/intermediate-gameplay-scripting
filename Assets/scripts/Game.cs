469a60eb756da2571274a52768f4be043600371d
John McIntosh II <jmci10@hotmail.com>
2017-01-13 22:06:13 -0500

    use unity properties

---
+++ Assets/scripts/Game.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	void Start() {
+		Player myPlayer = new Player();
+
+		//properties can be used just like variables
+		myPlayer.Experience = 5;
+		int x = myPlayer.Experience;
+	}
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player {
+
+	//member variables can be referred to as fields
+	private int experience;
+
+	//experience is a basic property
+	public int Experience {
+		get {
+			//some other code
+			return experience;
+		}
+		set {
+			//some other code
+			experience = value;
+		}
+	}
+
+	//level is a property that converts experience
+	//points into the level of a player automatically
+	public int Level {
+		get {
+			return experience / 1000;
+		}
+		set {
+			experience = value * 1000;
+		}
+	}
+
+	//this is an example of an auto-implemented
+	//property
+	public int Health {
+		get;
+		set;
+	}
+}
