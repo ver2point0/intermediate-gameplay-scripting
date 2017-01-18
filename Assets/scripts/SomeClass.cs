87e2b5ff744f017cd27f6f0c08a81bd7a8645d78
John McIntosh II <jmci10@hotmail.com>
2017-01-18 16:21:30 -0500

    overload Add() method

---
+++ Assets/scripts/SomeClass.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SomeClass {
+
+	public int Add(int n1, int n2) {
+		return n1 + n2;
+	}
+
+	public string Add(string s1, string s2) {
+		return s1 + s2;
+	}
+}
---
+++ Assets/scripts/SomeOtherClass.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SomeOtherClass : MonoBehaviour {
+
+	void Start() {
+		SomeClass myClass = new SomeClass();
+		myClass.Add(1, 2);
+		myClass.Add("test", "exam");
+	}
+}
