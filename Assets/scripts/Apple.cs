b0c7ab3811cd7049e50a805f93a531874ac97ea9
John McIntosh II <jmci10@hotmail.com>
2017-01-20 20:08:11 -0500

    use inheritance to create a fruit salad

---
+++ Assets/scripts/Apple.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Apple : Fruit {
+
+	public Apple() {
+		color = "red";
+		Debug.Log("1st Apple Constructor Called");
+	}
+
+	public Apple(string newColor) : base(newColor) {
+		Debug.Log("2nd Apple Constructor Called");
+	}
+}
---
+++ Assets/scripts/FruitSalad.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FruitSalad : MonoBehaviour {
+
+	void Start() {
+		Debug.Log("Creating the fruit");
+		Fruit myFruit = new Fruit();
+		Debug.Log("Creating the apple");
+		Apple myApple = new Apple();
+
+		myFruit.SayHello();
+		myFruit.Chop();
+
+		myApple.SayHello();
+		myApple.Chop();
+
+		Debug.Log("Creating the fruit");
+		myFruit = new Fruit("yellow");
+		Debug.Log("Creating the apple");
+		myApple = new Apple("green");
+
+		myFruit.SayHello();
+		myFruit.Chop();
+
+		myApple.SayHello();
+		myApple.Chop();
+	}
+}
