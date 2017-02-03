2bc655c1ba606b5b062ad57a9f63192aad877cb8
John McIntosh II <jmci10@hotmail.com>
2017-02-02 21:20:57 -0500

    add additional behavior with attributes

---
+++ Assets/scripts/ColorScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ColorScript : MonoBehaviour {
+
+	void Start() {
+		GetComponent<Renderer>().sharedMaterial.color = Color.blue;
+	}
+}
